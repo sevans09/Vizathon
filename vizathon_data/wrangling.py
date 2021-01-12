d41ffcec6b567413af272f6f6e7017cd24f1ce8c
sejaldua <sejal.dua@tufts.edu>
2021-01-12 10:13:26 -0800

    jsons uploaded

--- vizathon_data/wrangling.py
+++ vizathon_data/wrangling.py
@@ -2,10 +2,10 @@ import os
 import pandas as pd
 import openpyxl
 
-years = ["2020"]
+years = [str(num) for num in range(2011, 2021, 1)]
 orig_df = pd.DataFrame()
-for subdir in years:
-    for root, _, files in os.walk("./" + subdir):
+for year in years:
+    for root, _, files in os.walk("./" + year):
         for file in files:
             fpath = os.path.join(root, file)
             if fpath[-4:] != ".xls" and fpath[-5:] != ".xlsx":
@@ -23,6 +23,4 @@ for subdir in years:
                 
                 df.drop(df.columns.difference(['FIPS', 'County', '% Adults with Obesity']), 1, inplace=True)
                 orig_df = pd.concat([orig_df, df], ignore_index=True)
-        orig_df.to_json("data_2020.json", orient='index')
-        break
-    break
+        orig_df.to_json("./../jsons/data_" + year + ".json", orient='index')
